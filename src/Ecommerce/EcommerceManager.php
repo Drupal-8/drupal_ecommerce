36f68c8bfc5a110f4baa0c20e7cf15e2797982b0
Manel <malotor@gmail.com>
2014-12-22 13:23:06 +0100

    Create Ecommerce Manager

---
+++ src/Ecommerce/EcommerceManager.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manel
+ * Date: 21/12/14
+ * Time: 21:02
+ */
+
+namespace Drupal\ecommerce\Ecommerce;
+
+
+class EcommerceManager  {
+
+  protected $productDAO;
+  protected $cartDAO;
+
+  public function __construct(ProductDAOInterface $productDAO, CartDAOInterface $cartDAO) {
+    $this->productDAO = $productDAO;
+    $this->cartDAO = $cartDAO;
+  }
+
+  public function addProductToCart($productId) {
+    $product = $this->productDAO->get($productId);
+    $shoppingCart = $this->cartDAO->get();
+    $shoppingCart->addItem(CartLine::create($product,1));
+    $this->cartDAO->save($shoppingCart);
+  }
+
+}
