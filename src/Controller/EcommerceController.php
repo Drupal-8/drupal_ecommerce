1f7d5fd4e7edb62dc394499e496f81fe3e1b0947
Manel <malotor@gmail.com>
2014-12-22 13:24:37 +0100

    Use EcommerceManager in controller

--- src/Controller/EcommerceController.php
+++ src/Controller/EcommerceController.php
@@ -7,35 +7,34 @@ namespace Drupal\ecommerce\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-use Drupal\ecommerce\Entity\Product;
-use Drupal\ecommerce\Ecommerce\ProductDAO;
-use Drupal\ecommerce\Ecommerce\CartLine;
+use Drupal\ecommerce\Ecommerce\EcommerceManager;
+
 use Drupal\ecommerce\Ecommerce\CartDAO;
 use Drupal\ecommerce\Ecommerce\Printer;
+
+use Drupal\ecommerce\Ecommerce\CartDAOInterface;
+use Drupal\ecommerce\Ecommerce\ProductDAOInterface;
+
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class EcommerceController extends ControllerBase {
 
+  public function __construct(ProductDAOInterface $productDAO, CartDAOInterface $cartDAO) {
+    $this->productDAO = $productDAO;
+    $this->cartDAO = $cartDAO;
+  }
 
   public function addToCart($productId) {
     try {
 
-      $productDAO = \Drupal::service('ecommerce.product_dao');
-
-      //@todo test that id is not null
-      $product = $productDAO::get($productId);
-
-      $shoppingCart = CartDAO::get();
-
-      $shoppingCart->addItem(CartLine::create($product,1));
-
-      CartDAO::save($shoppingCart);
+      $ecommerceManager = new EcommerceManager($this->productDAO, $this->cartDAO);
+      $ecommerceManager->addProductToCart($productId);
 
       //Redirect to previous page
       $request = \Drupal::request();
       $referer = $request->headers->get('referer');
       return RedirectResponse::create($referer);
-
 
     } catch (\Exception $e) {
       drupal_set_message ($e->getMessage (), 'error');
@@ -51,5 +50,14 @@ class EcommerceController extends ControllerBase {
     }
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('ecommerce.product_dao'),
+      $container->get('ecommerce.cart_dao')
+    );
+  }
 
 }
