564d34a4f729cdc3076f1e9b1fbafa33918b592c
Manel <malotor@gmail.com>
2015-01-11 20:54:34 +0100

    Refactoring LineCarts

--- src/Ecommerce/Adapters/CartDBRepository.php
+++ src/Ecommerce/Adapters/CartDBRepository.php
@@ -2,55 +2,20 @@
 
 namespace Drupal\ecommerce\Ecommerce\Adapters;
 
-//use Symfony\Component\HttpFoundation\Session\Session;
-
 use malotor\ecommerce\Adapters\CartRepositoryInterface;
-use malotor\ecommerce\CartLine;
 use malotor\ecommerce\Cart;
 
 class CartDBRepository implements CartRepositoryInterface {
 
-  private $userId;
 
-  public function __construct($current_user, $productRepository) {
-    $this->account = $current_user;
-    $this->productRepository = $productRepository;
-    $this->userId = $this->account->id();
+  public function __construct($cartLineRepository) {
+    $this->cartLineRepository = $cartLineRepository;
   }
 
-  private function getCartLines() {
-    $result = db_select('CartLine', 'c')
-      ->fields('c')
-      ->condition('c.user_id', $this->userId)
-      ->execute();
-    $cartLines = [];
-    foreach ($result as $record) {
-      $cartLines[] = CartLine::create($this->productRepository->getProductByReference($record->item_id), $record->item_quantity);
-    }
-    return $cartLines;
-  }
-
-  private function deleteCartLines() {
-    db_delete('CartLine')
-      ->condition('user_id', $this->userId)
-      ->execute();
-  }
-
-  private function insertCartLines($cartline) {
-
-    $fields = array(
-      'user_id' => $this->userId,
-      'item_id' => $cartline->getItemReference(),
-      'item_quantity' => $cartline->getQuantity(),
-    );
-    db_insert('CartLine')
-      ->fields($fields)
-      ->execute();
-  }
 
   public function get() {
 
-    $carLines = $this->getCartLines();
+    $carLines = $this->cartLineRepository->getCartLines();
     $cart = new Cart();
     foreach ($carLines as $carLine) {
       $cart->addCartLine($carLine);
@@ -61,10 +26,10 @@ class CartDBRepository implements CartRepositoryInterface {
 
   public function save($shoppingCart) {
 
-    $this->deleteCartLines();
+    $this->cartLineRepository->deleteCartLines();
     $chartIterator = $shoppingCart->getIterator();
     foreach ($chartIterator as $cartline) {
-      $this->insertCartLines($cartline);
+      $this->cartLineRepository->insertCartLines($cartline);
     }
   }
 
---
+++ src/Ecommerce/Adapters/CartLineRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\ecommerce\Ecommerce\Adapters;
+
+
+class CartLineRepository {
+
+  public function __construct($current_user, $productRepository) {
+    $this->account = $current_user;
+    $this->productRepository = $productRepository;
+    $this->userId = $this->account->id();
+  }
+
+  public function getCartLines() {
+    $result = db_select('CartLine', 'c')
+      ->fields('c')
+      ->condition('c.user_id', $this->userId)
+      ->execute();
+    $cartLines = [];
+    foreach ($result as $record) {
+      $cartLines[] = CartLine::create($this->productRepository->getProductByReference($record->item_id), $record->item_quantity);
+    }
+    return $cartLines;
+  }
+
+  public function deleteCartLines() {
+    db_delete('CartLine')
+      ->condition('user_id', $this->userId)
+      ->execute();
+  }
+
+  public function insertCartLines($cartline) {
+
+    $fields = array(
+      'user_id' => $this->userId,
+      'item_id' => $cartline->getItemReference(),
+      'item_quantity' => $cartline->getQuantity(),
+    );
+    db_insert('CartLine')
+      ->fields($fields)
+      ->execute();
+  }
+} 
